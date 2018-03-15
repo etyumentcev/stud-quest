7db911210d65361d39219e782cb358bd37b0f548
Volkov Ilia <volkov-i1799@mail.ru>
2018-03-15 18:38:09 +0600

    one

---
+++ Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+class Character
+{
+	
+	int health;
+	int damage;
+	int c;
+public:
+	
+};
+
+
+class Monster :public Character
+{
+public:
+
+};
+
+
+class Hero :public Character
+{
+	vector<int> scores;
+public:
+
+};
+
+
+void main()
+{
+	
+	system("pause");
+}
+
