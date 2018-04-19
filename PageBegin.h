04828e6bd2d6675b0426b958ce60e26a4e6ce5da
VolkovI99 <37182489+VolkovI99@users.noreply.github.com>
2018-04-19 23:36:41 +0600

    Create PageBegin.h

---
+++ PageBegin.h
@@ -0,0 +1,24 @@
+#ifndef _PAGEBEGIN_H_
+#define _PAGEBEGIN_H_
+#include "BasisDraw.h"
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+class PageBegin : public BasisDraw
+{
+	//int current, action;
+	//vector <map <int, int> > gr;
+
+public:
+
+	virtual void Draw();
+
+};
+
+
+
+
+#endif
