95dd0e324749502fe7846311dce5b5c821137e02
VolkovI99 <37182489+VolkovI99@users.noreply.github.com>
2018-04-26 22:19:06 +0600

    Create PageBegin.cpp

---
+++ PageBegin.cpp
@@ -0,0 +1,35 @@
+#include "PageBegin.h"
+#include <windows.h>
+
+
+PageBegin::PageBegin():choose()
+{
+
+}
+
+void PageBegin::Draw()
+{
+	
+
+	/*COORD coord;
+	coord.X = 1;
+	coord.Y = 1;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);*/
+
+	cout << "Ваше имя?" << endl;
+	cin >> Name;
+	cout << "Состоялось слушанье по вашему делу."<<endl<<"Присяжные единогласно решили, что вы виновны."<< endl ;
+	cout << "И теперь вы, "<< Name<<", прибывете в тюрьму вместе с остальными заключенными"<<endl<< "и ваши будни в неволе начинаются.";
+	
+	cout <<endl<<"1)Принять свою участь."<<endl<<"2)Попытаться сбежать."<<endl;
+	cin >> choose;
+	
+
+
+
+	
+	
+	
+	
+	//current = gr[current][action];
+}
