653811d87ac186141b5bcb94221878254a8495fa
VolkovI99 <37182489+VolkovI99@users.noreply.github.com>
2018-04-26 22:18:28 +0600

    Create Page1.h

---
+++ Page1.h
@@ -0,0 +1,23 @@
+#ifndef _PAGE1_H_
+#define _PAGE1_H_
+#include "BasisDraw.h"
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+class Page1 : public BasisDraw
+{
+	
+
+public:
+
+	virtual void Draw();
+
+};
+
+
+
+
+#endif
