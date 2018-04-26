b010aca6bdd2008523acfda790e5261e20872001
VolkovI99 <37182489+VolkovI99@users.noreply.github.com>
2018-04-26 22:20:03 +0600

    Create PageGameOver.h

---
+++ PageGameOver.h
@@ -0,0 +1,23 @@
+#ifndef _PAGEGAMEOVER_H_
+#define _PAGEGAMEOVER_H_
+#include "BasisDraw.h"
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+class PageGameOver : public BasisDraw
+{
+
+
+public:
+
+	virtual void Draw();
+
+};
+
+
+
+
+#endif
