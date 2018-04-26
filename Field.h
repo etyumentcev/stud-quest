358692173a1f37521378dccd309f04ae3962b0c0
VolkovI99 <37182489+VolkovI99@users.noreply.github.com>
2018-04-26 22:17:09 +0600

    Update Field.h

--- Field.h
+++ Field.h
@@ -2,16 +2,25 @@
 #define _FIELD_H_
 #include "BasisDraw.h"
 #include "Background.h"
-
+#include "PageBegin.h"
+#include "PageGameOver.h"
+#include "Page1.h"
 class Field : public BasisDraw
 {
 
 	BasisDraw* background;
+
+	vector<BasisDraw*>pages;
+
+	int current;
+	string Name;
+	int choose;
+	
 	
 
 public:
 
-	Field(BasisDraw* background);
+	Field(BasisDraw* background,vector<BasisDraw*> pages);
 
 	virtual void Draw();
 	
