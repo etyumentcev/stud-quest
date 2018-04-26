e36a0c3233ce741e3cf03774b5806912bfda74cb
VolkovI99 <37182489+VolkovI99@users.noreply.github.com>
2018-04-26 22:17:28 +0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,19 @@
 #include <iostream>
 
 
-
 void main()
-{	
-	
+{
+	setlocale(LC_ALL, "ru");
 
+	vector <BasisDraw*> pages;
+	pages.push_back(new PageBegin);
+	pages.push_back(new PageGameOver);
+	//pages.push_back(new Page1);
+	//Field f = new (Background,pages);
+	Field f(new Background, pages);
+	
+	f.Draw();
+
+	
 	system("pause");
 }
